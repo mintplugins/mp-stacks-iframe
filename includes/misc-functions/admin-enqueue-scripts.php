281bd35370b42d6046b255f06736655a38553199
Phil Johnston <support@mintplugins.com>
2015-09-21 10:15:16 -0400

    Admin Meta Scripts now enqueued only when needed.

---
+++ includes/misc-functions/admin-enqueue-scripts.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file contains the enqueue scripts function for the iframe plugin
+ *
+ * @since 1.0.0
+ *
+ * @package    MP Stacks iFrame
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2015, Mint Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
+ */
